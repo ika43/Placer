a2bd86464cc91fdd82255b60005f8a27585fc08b
razzil1 <dusanpilipovic95@gmail.com>
2019-03-10 14:48:54 +0100

    search form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
 import { NgRedux } from 'ng2-redux';
 import { IApartmentState } from './store';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 const log = console.log;
 
 @Component({
@@ -13,13 +15,20 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private homeService: HomeService,
-    private ngRedux: NgRedux<IApartmentState>
+    private ngRedux: NgRedux<IApartmentState>,
+    private router: Router
   ) { }
 
   apartments: [];
   recommendedCities;
   recommendedApartmentsInBelgrade;
   loading: Boolean;
+
+  searchForm = new FormGroup({
+    'location': new FormControl(),
+    'dateRange': new FormControl(),
+    'numberOfVisitors': new FormControl()
+  })
 
   ngOnInit() {
     log("COMPONENT INIT: HOME");
@@ -34,6 +43,20 @@ export class HomeComponent implements OnInit {
         //  console.log("item", item)
         //})
       })
+  }
+
+  get dateRange() {
+    return this.searchForm.get('dateRange');
+  }
+  get location() {
+    return this.searchForm.get('location');
+  }
+  get numberOfVisitors() {
+    return this.searchForm.get('numberOfVisitors');
+  }
+
+  submitSearch() {
+    this.router.navigate([`/apartments/city/${this.location.value}`], { queryParams: { page: 0 } });
   }
 
 }
